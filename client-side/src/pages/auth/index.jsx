3147ca4602c69c8aca7feb3d3a30e7315a605a79
Vishal Singh <singhvishal2977@gmail.com>
2025-07-24 01:02:12 +0530

    Add Favicon, create header

--- client-side/src/pages/auth/index.jsx
+++ client-side/src/pages/auth/index.jsx
@@ -1,10 +1,11 @@
+import Component from "@/components/comp-577";
 import React from "react";
 
 export default function AuthPage() {
   return (
     <>
-          <section className="flex font-clash text-red-800 text-center font-extrabold text-8xl flex-col min-h-screen">
-              Hello
+      <section className="min-h-screen">
+        <Component/>
       </section>
     </>
   );
